def59f2db04ff85d72e2f97970c71eb87f0aef80
sanjarbey <64259769+sanjarbey@users.noreply.github.com>
2025-02-17 16:01:44 +0500

    oson qil

--- core/settings.py
+++ core/settings.py
@@ -117,10 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static fayllar uchun sozlamalar
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Statik fayllar joylashuvi
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Production uchun
 
+# Media fayllar (foydalanuvchilar yuklagan rasmlar, fayllar)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # Media fayllar joylashuvi
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('blog.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# STATIC va MEDIA fayllarni faqat "DEBUG=True" bo'lsa ishlatish
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
