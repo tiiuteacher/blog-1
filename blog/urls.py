7a1346af33daf660bf5579e169a7b36954c88553
sanjarbey <64259769+sanjarbey@users.noreply.github.com>
2025-02-18 14:30:16 +0500

    boshladik

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
     path('team/', views.team, name='team'),
+    path('contact/', views.contact, name='contact'),
 ]
 
